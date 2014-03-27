aa884b115c7746dd7dab90fecaad185e6de92f9c
Luc Bourlier <luc.bourlier@typesafe.com>
2014-03-28 00:09:55 +0100

    Update of longknife sync script

--- devices/longknife/sync.sh
+++ devices/longknife/sync.sh
@@ -2,6 +2,16 @@
 
 echo ">>>> Synching files for longknife"
 
+echo ">> /etc"
+cp /etc/portage/make.conf etc/portage/make.conf
+cp /etc/portage/package.accept_keywords etc/portage/package.accept_keywords
+cp /etc/conf.d/hostname etc/conf.d/hostname
+cp /etc/conf.d/keymaps etc/conf.d/keymaps
+cp /etc/env.d/02locale etc/env.d/02locale
+cp /etc/timezone etc/timezone
+cp /etc/hosts etc/hosts
+cp /etc/locale.gen etc/locale.gen
+
 echo ">> kernel config"
 cp /usr/src/linux/.config kernel/.config
 
