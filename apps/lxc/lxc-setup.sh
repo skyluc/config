9dd95b5022929270920a406decaa2b5fec826ec4
Luc Bourlier <luc.bourlier@typesafe.com>
2016-10-18 15:34:24 +0200

    Fixes in lxc script

--- apps/lxc/lxc-setup.sh
+++ apps/lxc/lxc-setup.sh
@@ -8,10 +8,13 @@ lxc-attach -n $CONTAINER_NAME -- apt install -y openssh-server
 lxc-attach -n $CONTAINER_NAME -- mkdir /home/ubuntu/.ssh
 lxc-attach -n $CONTAINER_NAME -- bash -c "echo '$PUBLIC_KEY' >> /home/ubuntu/.ssh/authorized_keys"
 lxc-attach -n $CONTAINER_NAME -- chown ubuntu:ubuntu /home/ubuntu/.ssh
-lxc-attach -n $CONTAINER_NAME -- sed -i 's/%sudo ALL=(ALL:ALL) ALL/%sudo ALL=(ALL:ALL) NOPASSWD:ALL/' /etc/sudoers
+lxc-attach -n $CONTAINER_NAME -- sed -i 's/%sudo\(.*\)ALL=(ALL:ALL) ALL/%sudo\1ALL=(ALL:ALL) NOPASSWD:ALL/' /etc/sudoers
 
 #TODO: private key, for github?
 
 CONTAINER_IP=$(lxc-ls -f | grep $CONTAINER_NAME | awk '{print $5}')
 
 sudo sed -i "s/.* $CONTAINER_NAME/$CONTAINER_IP $CONTAINER_NAME/" /etc/hosts
+
+# clean know_hosts if needed
+ssh-keygen -f "/home/luc/.ssh/known_hosts" -R $CONTAINER_NAME
